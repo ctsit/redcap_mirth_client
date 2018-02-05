efc4da43f1d0025575b4019680c72ed9de42a752
Dileep Rajput <dileep_rajput@yahoo.com>
2018-02-04 22:27:20 -0500

    Add methods to REDCapMirthClient class to send http requests via the Guzzle library

--- REDCapMirthClient.php
+++ REDCapMirthClient.php
@@ -6,9 +6,38 @@
 
 namespace REDCapMirthClient;
 
+require 'vendor/autoload.php';
+use GuzzleHttp\Client;
+
+
 /**
  * REDCap Mirth Connect client class.
  */
 class REDCapMirthClient {
-    // TODO.
+
+	private $client;
+
+	function __construct($base_url) {
+		$this->client = new Client(['base_uri' => $base_url]);
+	}
+
+	function request($method, $extension, $body) {
+		return $this->client->request($method, $extension, ["body" => $body]);
+	}
+
+	function get($extension) {
+		return $this->client->request('GET', $extension);
+	}
+
+	function delete($extension) {
+		return $this->client->request('DELETE', $extension);
+	}
+
+	function post($extension, $body) {
+		return $this->client->request('POST', $extension, ["body" => $body]);
+	}
+
+	function put($extension, $body) {
+		return $this->client->request('PUT', $extension, ["body" => $body]);
+	}
 }
