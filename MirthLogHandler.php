4e9ea239808d9138d07177960a32292fbab39f67
Dileep <rajputd@ufl.edu>
2018-02-07 10:16:28 -0500

    Add method to MirthLogHandler to parse data from log messages

--- MirthLogHandler.php
+++ MirthLogHandler.php
@@ -14,6 +14,20 @@ class MirthLogHandler extends AbstractProcessingHandler {
 
   }
 
+  //parses the backslash delimeted log message for the desired data
+  private function parse_message($message) {
+    preg_match_all('/([^\\\\]+)/', $message, $matches);
+    $matches = $matches[0];
+    return array(
+      'method' => $matches[0],
+      'uri' => $matches[1],
+      'version' => $matches[2],
+      'status_code' => $matches[3],
+      'request' => $matches[4],
+      'response' => $matches[5]
+    );
+  }
+
 }
 
 ?>
