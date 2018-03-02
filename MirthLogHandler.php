5378f9f59f937167c28cd358826509798c7e7fe4
Dileep <rajputd@ufl.edu>
2018-03-02 11:19:12 -0500

    Add amend_last_log method to MirthLogHandler

--- MirthLogHandler.php
+++ MirthLogHandler.php
@@ -14,20 +14,19 @@ class MirthLogHandler extends AbstractProcessingHandler {
     $this->project_id = $project_id;
   }
 
-  protected function write(array $record) {
-    //ignore log messages with the following level, they will be written later
-    //with a different level due to error handling reasons.
-    if ($record['level'] == 250) {
-      return;
-    }
+  public function amend_last_log($values) {
+    $query = $this->construct_amend_query($values);
+    AbstractExternalModule::query($query);
+  }
 
+  protected function write(array $record) {
     //build and consolidate data to store in db
     $data = $this->parse_message($record['message']);
     $data['project_id'] = $this->project_id;
     $data['datetime'] = $record['datetime']->format('Y-m-d H:i:s');
 
     //store in db
-    $query = $this->construct_query($data);
+    $query = $this->construct_write_query($data);
     AbstractExternalModule::query($query);
   }
 
@@ -44,7 +43,21 @@ class MirthLogHandler extends AbstractProcessingHandler {
     );
   }
 
-  private function construct_query(array $data) {
+  private function construct_amend_query($values) {
+    $query = "UPDATE redcap_mirth_client_log SET ";
+
+    foreach($values as $field => $value) {
+      $query .= "$field = '$value',";
+    }
+
+    //remove last comma
+    $query = substr_replace($query, "", -1);
+    $query .=  " WHERE project_id='" . $this->project_id . "'";
+    $query .= " ORDER BY datetime DESC LIMIT 1";
+    return $query;
+  }
+
+  private function construct_write_query($data) {
     extract($data);
     return "INSERT INTO redcap_mirth_client_log (project_id, method, uri, status_code, request, response, datetime) VALUES ('$project_id', '$method', '$uri', '$status_code', '$request', '$response', '$datetime')";
   }
@@ -57,7 +70,7 @@ class MirthLogHandler extends AbstractProcessingHandler {
         status_code VARCHAR(3),
         request TEXT,
         response TEXT,
-        datetime TIMESTAMP
+        datetime TIMESTAMP NOT NULL default CURRENT_TIMESTAMP
     )");
   }
 
